e9d2c8cb503e852ffd7823f6b124d408354333de
Margarita Trejo <m4rgosss@gmail.com>
2022-11-15 23:22:06 -0700

    Terminacion Cotizacion

--- js/script.js
+++ js/script.js
@@ -22,46 +22,26 @@ const calcularBtn = () => {
 
 	switch (opcion) {
 		case "0":
-<<<<<<< HEAD
 			interes = 1.125;
 			meses = 12;
 			break;
 		case "1":
 			interes = 1.172;
-=======
-			interes = 0.125;
-			meses = 12;
-			break;
-		case "1":
-			interes = 0.172;
->>>>>>> 9d41cc54790b5676895176fb9e1a6b4a9051b52a
 			meses = 18;
 			break;
 
 		case "2":
-<<<<<<< HEAD
 			interes = 1.21;
-=======
-			interes = 0.21;
->>>>>>> 9d41cc54790b5676895176fb9e1a6b4a9051b52a
 			meses = 24;
 			break;
 
 		case "3":
-<<<<<<< HEAD
 			interes = 1.26;
-=======
-			interes = 0.26;
->>>>>>> 9d41cc54790b5676895176fb9e1a6b4a9051b52a
 			meses = 36;
 			break;
 
 		case "4":
-<<<<<<< HEAD
 			interes = 1.45;
-=======
-			interes = 0.45;
->>>>>>> 9d41cc54790b5676895176fb9e1a6b4a9051b52a
 			meses = 48;
 			break;
 
@@ -70,7 +50,6 @@ const calcularBtn = () => {
 			return alert("Seleccione una opcion valida");
 	}
 
-<<<<<<< HEAD
     let res = (valor - (valor * .30));
 
 	// Calcular resultados y mostrarlos
@@ -80,15 +59,6 @@ const calcularBtn = () => {
 	const financiar = document.getElementById("totalFinanciar").value;
 	//se actualizo el calculo de pago en meses
 	document.getElementById("pagoMensual").value = ((res*interes) / meses).toFixed(2);
-=======
-	// Calcular resultados y mostrarlos
-	document.getElementById("enganche").value = (valor * 0.3).toFixed(2);
-	const enganche = document.getElementById("enganche").value;
-	document.getElementById("totalFinanciar").value = ((valor - enganche) + (valor * interes)).toFixed(2);
-	const financiar = document.getElementById("totalFinanciar").value;
-	//se actualizo el calculo de pago en meses
-	document.getElementById("pagoMensual").value = (valor / meses).toFixed(2);
->>>>>>> 9d41cc54790b5676895176fb9e1a6b4a9051b52a
 };
 
 document.getElementById("btnCalcular").addEventListener("click",calcularBtn);
